c8abe5d125e5f351ea2eff2eeaf3c7000142c7fb
palliotte <114391946+palliotte@users.noreply.github.com>
2023-02-05 08:40:05 +0800

    Mise à jour main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-def apple_random():
-    global index, ok
-    while ok == 0:
-        index = 0
-        while index <= len(RINGS)-1:
-            if apple.is_touching(RINGS[index]):
-                apple.set(LedSpriteProperty.X, randint(0, 4))
-                apple.set(LedSpriteProperty.Y, randint(0, 4))
-            else:
-                ok = 1
-            index += 1
+
 def on_button_pressed_a():
     global directionx, directiony
     directionx = 0
@@ -33,9 +23,18 @@ def on_gesture_tilt_right():
     directiony = 0
 input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
-index = 0
-ok = 0
-apple: game.LedSprite = None
+def apple():
+    ok = 0
+    while ok ==0:
+        index = 0
+        for index in range(len.RINGS-1):
+            if apple.is_touching(RINGS[index]):
+                apple.set(LedSpriteProperty.X, randint(0, 4))
+                apple.set(LedSpriteProperty.Y, randint(0, 4))
+            else:
+                ok=1
+
+
 directiony = 0
 directionx = 0
 RINGS: List[game.LedSprite] = []
@@ -44,18 +43,16 @@ RINGS = [game.create_sprite(1, 2),game.create_sprite(0, 2)]
 directionx = 1
 directiony = 1
 apple = game.create_sprite(2, 2)
-ok = 0
-apple_random()
+apple()
+
 def on_forever():
     basic.pause(1000)
     RINGS.unshift(game.create_sprite(RINGS[0].get(LedSpriteProperty.X) + directionx,
             RINGS[0].get(LedSpriteProperty.Y) + directiony))
-#    if RINGS[0].is_touching_edge():
-#        game.game_over()
     if RINGS[0].is_touching(apple):
+        apple()
         game.add_score(1)
-        apple_random()
-    else:
+    else:    
         RINGS[RINGS.length-1].delete()
         RINGS.pop()
 basic.forever(on_forever)
