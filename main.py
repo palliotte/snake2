cbbe4b48f11601ba0991dbd2e304efdf3582d85d
palliotte <114391946+palliotte@users.noreply.github.com>
2023-02-06 18:35:03 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,58 +1,56 @@
-
 def on_button_pressed_a():
-    global directionx, directiony
+    global directionx, directiony, direx, direy
     directionx = 0
     directiony = 1
+    direx = directionx
+    direy = directiony
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_gesture_tilt_left():
-    global directionx, directiony
+    global direx, direy, directionx, directiony
+    direx = directionx
+    direy = directiony
     directionx = -1
     directiony = 0
 input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
 
 def on_button_pressed_b():
-    global directionx, directiony
+    global direx, direy, directionx, directiony
+    direx = directionx
+    direy = directiony
     directionx = 0
     directiony = -1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_tilt_right():
-    global directionx, directiony
+    global direx, direy, directionx, directiony
+    direx = directionx
+    direy = directiony
     directionx = 1
     directiony = 0
 input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
-
-def apple():
+def rapple():
     ok = 0
-    while ok ==0:
-        index = 0
-        for index in range(len.RINGS-1):
-            if apple.is_touching(RINGS[index]):
-                apple.set(LedSpriteProperty.X, randint(0, 4))
-                apple.set(LedSpriteProperty.Y, randint(0, 4))
-            else:
-                ok=1
-
-
+    while ok == 0:
+        n = 0
+        for n in range(0, RINGS.length-1):
+            apple.set(LedSpriteProperty.X, randint(0, 4))
+            apple.set(LedSpriteProperty.Y, randint(0, 4))
+            if not apple.is_touching(RINGS[n]):
+                ok = 1
+direy = 0
+direx = 1
 directiony = 0
-directionx = 0
-RINGS: List[game.LedSprite] = []
-game.set_score(0)
-RINGS = [game.create_sprite(1, 2),game.create_sprite(0, 2)]
 directionx = 1
-directiony = 1
+game.set_score(0)
+RINGS = [game.create_sprite(1, 2), game.create_sprite(0, 2)]
 apple = game.create_sprite(2, 2)
-apple()
-
+rapple()
 def on_forever():
+    global directionx, directiony
     basic.pause(1000)
     RINGS.unshift(game.create_sprite(RINGS[0].get(LedSpriteProperty.X) + directionx,
             RINGS[0].get(LedSpriteProperty.Y) + directiony))
-    if RINGS[0].is_touching(apple):
-        apple()
-        game.add_score(1)
-    else:    
-        RINGS[RINGS.length-1].delete()
-        RINGS.pop()
+    RINGS[len(RINGS) - 1].delete()
+    RINGS.remove_at(len(RINGS) - 1)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,59 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    directionx = 0
+    directiony = 1
+    direx = directionx
+    direy = directiony
+})
+input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+    
+    direx = directionx
+    direy = directiony
+    directionx = -1
+    directiony = 0
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    direx = directionx
+    direy = directiony
+    directionx = 0
+    directiony = -1
+})
+input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+    
+    direx = directionx
+    direy = directiony
+    directionx = 1
+    directiony = 0
+})
+function rapple() {
+    let n: number;
+    let ok = 0
+    while (ok == 0) {
+        n = 0
+        for (n = 0; n < RINGS.length - 1; n++) {
+            apple.set(LedSpriteProperty.X, randint(0, 4))
+            apple.set(LedSpriteProperty.Y, randint(0, 4))
+            if (!apple.isTouching(RINGS[n])) {
+                ok = 1
+            }
+            
+        }
+    }
+}
 
+let direy = 0
+let direx = 1
+let directiony = 0
+let directionx = 1
+game.setScore(0)
+let RINGS = [game.createSprite(1, 2), game.createSprite(0, 2)]
+let apple = game.createSprite(2, 2)
+rapple()
+basic.forever(function on_forever() {
+    
+    basic.pause(1000)
+    RINGS.unshift(game.createSprite(RINGS[0].get(LedSpriteProperty.X) + directionx, RINGS[0].get(LedSpriteProperty.Y) + directiony))
+    RINGS[RINGS.length - 1].delete()
+    RINGS.removeAt(RINGS.length - 1)
+})
