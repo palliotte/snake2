1f5fad4f32bd41ccd30ccdc429ee5793d1dce7c1
palliotte <114391946+palliotte@users.noreply.github.com>
2023-02-09 08:43:33 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,60 +1,63 @@
+def rapple():
+    global n
+    ok = 0
+    apple.set(LedSpriteProperty.X, randint(0, 4))
+    apple.set(LedSpriteProperty.Y, randint(0, 4))
+    while ok == 0:
+        n = 0
+        index = 0
+        while index <= len(RINGS) - 1:
+            if RINGS[n].is_touching(apple):
+                apple.set(LedSpriteProperty.X, randint(0, 4))
+                apple.set(LedSpriteProperty.Y, randint(0, 4))
+            else:
+                pass
+            index += 1
+
 def on_button_pressed_a():
-    global directionx, directiony, direx, direy
-    directionx = 0
-    directiony = 1
-    direx = directionx
-    direy = directiony
+    global directionx, directiony
+    if not (directionx == 0) and not (directiony == -1):
+        directionx = 0
+        directiony = 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_gesture_tilt_left():
-    global direx, direy, directionx, directiony
-    direx = directionx
-    direy = directiony
-    directionx = -1
-    directiony = 0
+    global directionx, directiony
+    if not (directionx == 1) and not (directiony == 0):
+        directionx = -1
+        directiony = 0
 input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
 
 def on_button_pressed_b():
-    global direx, direy, directionx, directiony
-    direx = directionx
-    direy = directiony
-    directionx = 0
-    directiony = -1
+    global directionx, directiony
+    if not (directionx == 0) and not (directiony == 1):
+        directionx = 0
+        directiony = -1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_tilt_right():
-    global direx, direy, directionx, directiony
-    direx = directionx
-    direy = directiony
-    directionx = 1
-    directiony = 0
+    global directionx, directiony
+    if not (directionx == -1) and not (directiony == 0):
+        directionx = 1
+        directiony = 0
 input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
-def rapple():
-    ok = 0
-    while ok == 0:
-        n = 0
-        for n in range(0, RINGS.length-1):
-            apple.set(LedSpriteProperty.X, randint(0, 4))
-            apple.set(LedSpriteProperty.Y, randint(0, 4))
-            if not apple.is_touching(RINGS[n]):
-                ok = 1
-direy = 0
-direx = 1
-directiony = 0
+n = 0
+apple: game.LedSprite = None
+RINGS: List[game.LedSprite] = []
 directionx = 1
+directiony = 0
 game.set_score(0)
 RINGS = [game.create_sprite(1, 2), game.create_sprite(0, 2)]
 apple = game.create_sprite(2, 2)
 rapple()
 
 def on_forever():
-    global directionx, directiony
     basic.pause(1000)
-    RINGS.unshift(game.create_sprite(RINGS[0].get(LedSpriteProperty.X) + directionx,
-            RINGS[0].get(LedSpriteProperty.Y) + directiony))
+    RINGS.unshift(game.create_sprite(RINGS[0].get(LedSpriteProperty.X) + directionx,RINGS[0].get(LedSpriteProperty.Y) + directiony))
     if RINGS[0].is_touching_edge():
-        game.game_over()
+        if RINGS[0].get(LedSpriteProperty.X)==0 and directionx == -1 or RINGS[0].get(LedSpriteProperty.X)==4 and directionx == 1 or RINGS[0].get(LedSpriteProperty.Y)==0 and directiony == -1 or RINGS[0].get(LedSpriteProperty.Y)==4 and directionx == 1:
+            game.game_over()
     if RINGS[0].is_touching(apple):
         rapple()
         game.add_score(1)
--- main.ts
+++ main.ts
@@ -1,61 +1,74 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    directionx = 0
-    directiony = 1
-    direx = directionx
-    direy = directiony
-})
-input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
-    
-    direx = directionx
-    direy = directiony
-    directionx = -1
-    directiony = 0
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    direx = directionx
-    direy = directiony
-    directionx = 0
-    directiony = -1
-})
-input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
-    
-    direx = directionx
-    direy = directiony
-    directionx = 1
-    directiony = 0
-})
 function rapple() {
-    let n: number;
+    let index: number;
+    
     let ok = 0
+    apple.set(LedSpriteProperty.X, randint(0, 4))
+    apple.set(LedSpriteProperty.Y, randint(0, 4))
     while (ok == 0) {
         n = 0
-        for (n = 0; n < RINGS.length - 1; n++) {
-            apple.set(LedSpriteProperty.X, randint(0, 4))
-            apple.set(LedSpriteProperty.Y, randint(0, 4))
-            if (!apple.isTouching(RINGS[n])) {
-                ok = 1
+        index = 0
+        while (index <= RINGS.length - 1) {
+            if (RINGS[n].isTouching(apple)) {
+                apple.set(LedSpriteProperty.X, randint(0, 4))
+                apple.set(LedSpriteProperty.Y, randint(0, 4))
+            } else {
+                
             }
             
+            index += 1
         }
     }
 }
 
-let direy = 0
-let direx = 1
-let directiony = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (!(directionx == 0) && !(directiony == -1)) {
+        directionx = 0
+        directiony = 1
+    }
+    
+})
+input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+    
+    if (!(directionx == 1) && !(directiony == 0)) {
+        directionx = -1
+        directiony = 0
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (!(directionx == 0) && !(directiony == 1)) {
+        directionx = 0
+        directiony = -1
+    }
+    
+})
+input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+    
+    if (!(directionx == -1) && !(directiony == 0)) {
+        directionx = 1
+        directiony = 0
+    }
+    
+})
+let n = 0
+let apple : game.LedSprite = null
+let RINGS : game.LedSprite[] = []
 let directionx = 1
+let directiony = 0
 game.setScore(0)
-let RINGS = [game.createSprite(1, 2), game.createSprite(0, 2)]
-let apple = game.createSprite(2, 2)
+RINGS = [game.createSprite(1, 2), game.createSprite(0, 2)]
+apple = game.createSprite(2, 2)
 rapple()
 basic.forever(function on_forever() {
-    
     basic.pause(1000)
     RINGS.unshift(game.createSprite(RINGS[0].get(LedSpriteProperty.X) + directionx, RINGS[0].get(LedSpriteProperty.Y) + directiony))
     if (RINGS[0].isTouchingEdge()) {
-        game.gameOver()
+        if (RINGS[0].get(LedSpriteProperty.X) == 0 && directionx == -1 || RINGS[0].get(LedSpriteProperty.X) == 4 && directionx == 1 || RINGS[0].get(LedSpriteProperty.Y) == 0 && directiony == -1 || RINGS[0].get(LedSpriteProperty.Y) == 4 && directionx == 1) {
+            game.gameOver()
+        }
+        
     }
     
     if (RINGS[0].isTouching(apple)) {
